829a66bd2443991235b4f0977e01e1f0cc91ff36
Simone Colazzo <colsimo88@gmail.com>
2020-07-07 10:24:46 +0200

    Trying modify app mdouel

--- plopfile.js
+++ plopfile.js
@@ -1,14 +1,20 @@
 const glob = require('glob-promise');
+const fs = require('fs');
+const { cwd } = require('process');
+
+const SRC_DIR = 'src';
+const NG_MODULE = '@NgModule';
+const IMPORTS = 'imports';
+const STORE_MODULE = 'StoreModule';
 
 //If store module doesn't exists return false
 //If store module exists in the right folder returns true
 //If store module exists in a wrong folder exits the app
 function verifyStoreModule()
 {
-  var dirs = glob.sync('src/**/*');
+  var dirs = glob.sync(SRC_DIR + '/**/*');
 
   for (var i = 0; i < dirs.length; ++i)
-  {
     if (dirs[i].indexOf("store.module") >= 0)
     {
       if (dirs[i].indexOf("store/store.module") < 0)
@@ -19,9 +25,38 @@ function verifyStoreModule()
 
       return true;
     }
-  }
 
   return false;
+}
+
+function addImportToAppModule(path)
+{
+    var dirs = glob.sync(SRC_DIR + '/**/*');
+
+    var subpath;
+    for (var i = 0; i < dirs.length; ++i)
+        if (dirs[i].indexOf("app.module") >= 0)
+            subpath = dirs[i];
+
+    if (subpath)
+    {
+        var appModule = fs.readFileSync(path + '/' + subpath).toString();
+
+        const appModuleLines = appModule.split(/\r?\n/);
+        
+        var ngModuleLineIndex, importsLineIndex, storeModuleLineIndex;
+
+        for (var i = 0; i < appModuleLines; ++i)
+            if (appModuleLines[i].indexOf(NG_MODULE) >= 0)
+                ngModuleLineIndex = i;
+            else if (appModuleLines[i].indexOf(IMPORTS) >= 0)
+                importsLineIndex = i;
+            else if (appModuleLines[i].indexOf(STORE_MODULE) >= 0)
+                storeModuleLineIndex = i;
+
+        if (ngModuleLineIndex && importsLineIndex && importsLineIndex > ngModuleLineIndex)
+
+    }
 }
 
 module.exports = function (plop)
@@ -38,7 +73,7 @@ module.exports = function (plop)
                 type: 'input',
                 name: 'storeDir',
                 message: 'In which directory do you want to create the store folder?',
-                default: 'src'
+                default: SRC_DIR
             }, {
                 type: 'confirm',
                 name: 'enableEpics',
